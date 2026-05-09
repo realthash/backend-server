307b1b5966214f6b704b0053a135daf779502a8b
realthash <thashmikarathnayake11@gmail.com>
2026-05-09 22:22:21 +0530

    declare a get method

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,12 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+//HTTP VERBS and Routes
+/* The method informs the nature of request and the route is a further 
+subdirectory*/
+app.get('/', (req, res)=>{
+    console.log("this is an endpoint", req.method)
+    res.sendStatus(200)
+})
 
 app.listen(PORT, () => console.log(`server started to listen : ${PORT}`))
