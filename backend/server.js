af1c41fbbb2ca36d318c95a56bb6481a30f4c4fc
realthash <thashmikarathnayake11@gmail.com>
2026-05-10 07:00:16 +0530

    create website endpoints and API endpoits

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,29 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-//HTTP VERBS and Routes
+//define some data
+let data = {
+    name: 'thash'
+}
+
+
+//ENDPOINT - HTTP VERBS and Routes
 /* The method informs the nature of request and the route is a further 
 subdirectory*/
 app.get('/', (req, res)=>{
     console.log("this is an endpoint", req.method)
-    res.sendStatus(200)
+    res.send('<h1>home page</h1>')
+})
+
+// TYPE 1 - website endpoints (these endpoints are for sending back html and they typically come when a user enters a url in a browser)
+app.get('/dashboard', (req, res)=> {
+    res.send('<h1>dashboard</h1>')
+})
+
+// TYPE 2 - API endpoints
+app.get('/api/data', (req, res)=> {
+    console.log('this one was for data')
+    res.send(data)
 })
 
 app.listen(PORT, () => console.log(`server started to listen : ${PORT}`))
